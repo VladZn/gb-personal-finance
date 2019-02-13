33a6cd45ce3fa8442916b25900b1914682836f44
Pharmazon <as.shcheglov@gmail.com>
2019-02-13 21:06:36 +0300

    Minor additions and refactorings

--- pi-server-proxy/src/main/java/ru/gb/dev/spring/pi/server/proxy/PiServerProxyApplication.java
+++ pi-server-proxy/src/main/java/ru/gb/dev/spring/pi/server/proxy/PiServerProxyApplication.java
@@ -2,7 +2,9 @@ package ru.gb.dev.spring.pi.server.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class PiServerProxyApplication {
 
