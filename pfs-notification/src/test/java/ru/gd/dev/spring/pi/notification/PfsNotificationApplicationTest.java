1e66428e7b459c45c3fa112408c44075a0a5e3b6
Vlad Zinchenko <vzinchenko36@gmail.com>
2019-02-15 19:04:46 +0300

    changed prefix to pfs, added discovery client to config server

---
+++ pfs-notification/src/test/java/ru/gd/dev/spring/pi/notification/PfsNotificationApplicationTest.java
@@ -0,0 +1,15 @@
+package ru.gd.dev.spring.pi.notification;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author V. Zinchenko
+ */
+public class PfsNotificationApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
---
+++ pfs-server-discovery/src/main/java/ru/gb/dev/spring/pfs/server/discovery/PfsServerDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package ru.gb.dev.spring.pfs.server.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PfsServerDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfsServerDiscoveryApplication.class, args);
+    }
+
+}
