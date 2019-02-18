8868ceb4a692d7f9510619e6822da323272fb3b5
Vlad Zinchenko <vzinchenko36@gmail.com>
2019-02-18 18:42:31 +0300

    Added JWT tokens support (not finished yet)

--- pfs-notification/src/main/java/ru/gd/dev/spring/psf/notification/PfsNotificationApplication.java
+++ pfs-notification/src/main/java/ru/gd/dev/spring/psf/notification/PfsNotificationApplication.java
@@ -2,13 +2,26 @@ package ru.gd.dev.spring.psf.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableResourceServer
 public class PfsNotificationApplication {
+
+//    @Bean
+//    public Filter userContextFilter() {
+//        UserContextFilter userContextFilter = new UserContextFilter();
+//        return userContextFilter;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(PfsNotificationApplication.class, args);
     }
 }
+
+
