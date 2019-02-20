b8d503a6e6a2499d9a6a08da0fad452ad71f602e
Sergey Kubatko <skubatko@gmail.com>
2019-02-20 23:18:36 +0300

    feature/statistics: repository completed

--- pfs-statistics/src/main/java/ru/gb/dev/spring/pfs/statistics/model/repository/LogoRepository.java
+++ pfs-statistics/src/main/java/ru/gb/dev/spring/pfs/statistics/model/repository/LogoRepository.java
@@ -2,9 +2,9 @@ package ru.gb.dev.spring.pfs.statistics.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.gb.dev.spring.pfs.accounting.model.entity.Account;
+import ru.gb.dev.spring.pfs.statistics.model.entity.Logo;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, String> {
+public interface LogoRepository extends JpaRepository<Logo, String> {
 
 }
