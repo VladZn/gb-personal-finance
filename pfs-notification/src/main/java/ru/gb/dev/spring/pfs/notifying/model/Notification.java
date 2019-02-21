4ffb6f339c5f19833a078ae5f122a53166075383
Kogut Sergey <koguts@yandex.ru>
2019-02-21 15:18:17 +0300

    Убрал геттеры и сеттеры, откорректировал pom.xml, убрал лишние зависимости

--- pfs-notification/src/main/java/ru/gb/dev/spring/pfs/notifying/model/Notification.java
+++ pfs-notification/src/main/java/ru/gb/dev/spring/pfs/notifying/model/Notification.java
@@ -31,27 +31,4 @@ public class Notification extends AbstractBaseEntity {
         this.body = body;
     }
 
-    public Date getDate_time() {
-        return date_time;
-    }
-
-    public void setDate_time(Date date_time) {
-        this.date_time = date_time;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
 }
