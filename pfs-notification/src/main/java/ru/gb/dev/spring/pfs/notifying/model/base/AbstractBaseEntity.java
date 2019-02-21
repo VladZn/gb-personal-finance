5c1c3df5667e77868d2064cf5c7bf241c071d220
Kogut Sergey <koguts@yandex.ru>
2019-02-21 15:14:39 +0300

    Убрал геттеры и сеттеры, откорректировал pom.xml, убрал лишние зависимости

--- pfs-notification/src/main/java/ru/gb/dev/spring/pfs/notifying/model/base/AbstractBaseEntity.java
+++ pfs-notification/src/main/java/ru/gb/dev/spring/pfs/notifying/model/base/AbstractBaseEntity.java
@@ -23,27 +23,4 @@ public abstract class AbstractBaseEntity {
     @Column(name = "user_id", nullable = false)
     private String user_id;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
 }
