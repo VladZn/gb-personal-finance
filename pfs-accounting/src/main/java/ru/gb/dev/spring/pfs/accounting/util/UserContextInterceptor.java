0f10320dec4e0ce77e829bee02344d93905e1817
Mainframe\Vlad <vzinchenko36@gmail.com>
2019-02-25 01:16:23 +0300

    authentication

--- pfs-accounting/src/main/java/ru/gb/dev/spring/pfs/accounting/util/UserContextInterceptor.java
+++ pfs-accounting/src/main/java/ru/gb/dev/spring/pfs/accounting/util/UserContextInterceptor.java
@@ -14,10 +14,12 @@ import java.io.IOException;
 public class UserContextInterceptor implements ClientHttpRequestInterceptor {
 
     @Override
-    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+    public ClientHttpResponse intercept(HttpRequest request,
+                                        byte[] body,
+                                        ClientHttpRequestExecution execution) throws IOException {
         HttpHeaders headers = request.getHeaders();
-//        headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
-//        headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
+        headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
+        headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
 
         return execution.execute(request, body);
     }
